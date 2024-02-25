852dfe44fa0ef8623317966adca32fdd2419cb63
magitnup <lpunti@ope.at>
2024-02-25 17:19:11 +0100

    visible cards via js div creation - no colors yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Constants first meaning my playingfield, different cards/colors
 // and making sure that every color gets taken only twice
-const memogameContainer = document.querySelector("memo-game");
+const memogameContainer = document.querySelector(".memo-game");
 const colors = [
   "aqua",
   "crimson",
@@ -19,12 +19,21 @@ let openCount = 0;
 let activeCard = null;
 let awaitingEndOfMove = false;
 
+function makeCard(color) {
+  const element = document.createElement("div");
+
+  element.classList.add("cards");
+  element.setAttribute("data-color", color);
+
+  return element;
+}
+
 // Make them appear randomly + limit amount of the same color
 for (let i = 0; i < cardAnzahl; i++) {
   const randomIndex = Math.floor(Math.random() * chosenColor.length);
   const color = chosenColor[randomIndex];
+  const cards = makeCard(color);
 
   chosenColor.splice(randomIndex, 1);
-
-  console.log(color);
+  memogameContainer.appendChild(cards);
 }
